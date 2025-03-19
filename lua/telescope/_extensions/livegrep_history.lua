cc53f066852dd11b8ecd20c44b519c25adf998a2
happy663 <tatu2425@gmail.com>
2025-03-20 01:09:04 +0900

    feat: telescopeの拡張機能として履歴機能を追加

--- lua/telescope/_extensions/livegrep_history.lua
+++ lua/telescope/_extensions/livegrep_history.lua
@@ -1,6 +1,6 @@
 local has_telescope, telescope = pcall(require, "telescope")
 if not has_telescope then
-  error("This plugin requires nvim-telescope/telescope.nvim")
+	error("This plugin requires nvim-telescope/telescope.nvim")
 end
 
 local history = {}
@@ -8,88 +8,93 @@ local history_file = vim.fn.stdpath("data") .. "/telescope_livegrep_history.json
 local history_index = 0
 
 local function load_history()
-  local file = io.open(history_file, "r")
+	local file = io.open(history_file, "r")
 
-  if not file then
-    return {}
-  end
+	if not file then
+		return {}
+	end
 
-  if file then
-    local content = file:read("*a")
-    file:close()
-    if content and content ~= "" then
-      history = vim.fn.json_decode(content)
-    end
-  end
+	if file then
+		local content = file:read("*a")
+		file:close()
+		if content and content ~= "" then
+			history = vim.fn.json_decode(content)
+		end
+	end
 end
 
 local function save_history()
-  local file = io.open(history_file, "w")
-  if file then
-    file:write(vim.fn.json_encode(history))
-    file:close()
-  end
+	local file = io.open(history_file, "w")
+	if file then
+		file:write(vim.fn.json_encode(history))
+		file:close()
+	end
 end
 
 local function add_history(search_word)
-  table.insert(history, 1, search_word)
+	table.insert(history, 1, search_word)
 
-  -- 重複を削除
-  local hash = {}
-  history = vim.tbl_filter(function(value)
-    if not hash[value] then
-      hash[value] = true
-      return true
-    end
-    return false
-  end, history)
+	-- 重複を削除
+	local hash = {}
+	history = vim.tbl_filter(function(value)
+		if not hash[value] then
+			hash[value] = true
+			return true
+		end
+		return false
+	end, history)
 
-  -- 履歴が100件を超えたら一番古いものを削除
-  if #history > 100 then
-    table.remove(history)
-  end
+	-- 履歴が100件を超えたら一番古いものを削除
+	if #history > 100 then
+		table.remove(history)
+	end
 
-  save_history()
+	save_history()
 end
 
 local function live_grep_with_history()
-  local builtin = require("telescope.builtin")
-  local actions = require("telescope.actions")
-  local actions_state = require("telescope.actions.state")
+	local builtin = require("telescope.builtin")
+	local actions = require("telescope.actions")
+	local actions_state = require("telescope.actions.state")
 
-  load_history()
-  history_index = 0
+	load_history()
+	history_index = 0
 
-  builtin.live_grep({
-    prompt_title = "Live Grep History",
-    search = history,
-    attach_mappings = function(_, map)
-      map("i", "<Up>", function(_prompt_bufnr)
-        local picker = actions_state.get_current_picker(_prompt_bufnr)
-        if history_index < #history then
-          history_index = history_index + 1
-          picker:set_prompt(history[history_index])
-        end
-      end)
-      map("i", "<Down>", function(_prompt_bufnr)
-        local picker = actions_state.get_current_picker(_prompt_bufnr)
-        if history_index > 1 then
-          history_index = history_index - 1
-          picker:set_prompt(history[history_index])
-        elseif history_index == 1 then
-          history_index = 0
-          picker:set_prompt("")
-        end
-      end)
-      map("i", "<CR>", function(_prompt_bufnr)
-        local picker = actions_state.get_current_picker(_prompt_bufnr)
-        local search_word = picker:_get_prompt()
-        add_history(search_word)
-        history_index = 0
-        actions.select_default(_prompt_bufnr)
-      end)
-      return true
-    end,
-  })
+	builtin.live_grep({
+		prompt_title = "Live Grep History",
+		search = history,
+		attach_mappings = function(_, map)
+			map("i", "<Up>", function(_prompt_bufnr)
+				local picker = actions_state.get_current_picker(_prompt_bufnr)
+				if history_index < #history then
+					history_index = history_index + 1
+					picker:set_prompt(history[history_index])
+				end
+			end)
+			map("i", "<Down>", function(_prompt_bufnr)
+				local picker = actions_state.get_current_picker(_prompt_bufnr)
+				if history_index > 1 then
+					history_index = history_index - 1
+					picker:set_prompt(history[history_index])
+				elseif history_index == 1 then
+					history_index = 0
+					picker:set_prompt("")
+				end
+			end)
+			map("i", "<CR>", function(_prompt_bufnr)
+				local picker = actions_state.get_current_picker(_prompt_bufnr)
+				local search_word = picker:_get_prompt()
+				add_history(search_word)
+				history_index = 0
+				actions.select_default(_prompt_bufnr)
+			end)
+			return true
+		end,
+	})
 end
 
+return telescope.register_extension({
+	exports = {
+		live_grep_with_history = live_grep_with_history,
+	},
+})
